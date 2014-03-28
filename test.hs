3aa65d6c3d276ba7fa33249f0f23a2b42169e396
AlmasB <toll.everon@gmail.com>
2014-03-28 19:08:20 +0000

    hello world and basic stuff

---
+++ test.hs
@@ -0,0 +1,10 @@
+doubleMe x = x + x
+sumUs x y = x + y
+
+isEven x = if mod x 2 == 0
+            then True
+            else False
+            
+bdayList = [23, 29, 12]
+
+printHelloWorld = putStrLn "Hello World"
